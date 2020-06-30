3cad38b5ac4807780b2f0edce19115f0722a1da3
Edie Atha <edieatha@gmail.com>
2020-07-01 00:31:15 +0300

    1. Ran db:migrate 2. Update models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts
+  validates :username, length: { in: 4..12 }, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
