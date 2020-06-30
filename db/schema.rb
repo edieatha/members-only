8a091264c9a82b84c9291083d50670757a0783a6
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-07-01 02:18:43 +0300

    Add logic to index file to show either signout or sign in depending on status

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,16 @@
 ActiveRecord::Schema.define(version: 2020_06_30_220858) do
 
   create_table "posts", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
     t.string "title"
     t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "user_id"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
