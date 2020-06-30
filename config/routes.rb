514176ac041d41e283a87bd723090edfde3208a5
Edie Atha <edieatha@gmail.com>
2020-07-01 01:35:06 +0300

    Fixed route errors and missing methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   root 'posts#index'
   resources :posts, only: [:index, :create, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_202908) do
+ActiveRecord::Schema.define(version: 2020_06_30_220858) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.string "title"
+    t.text "content"
   end
 
   create_table "users", force: :cascade do |t|
