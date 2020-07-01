ec9d58e5bc275a995833e49a68cd86fe959dbd92
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-07-01 18:45:13 +0300

    Increase email length validation limit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
-  validates :email, length: { in: 4..12 }, presence: true, uniqueness: true
+  validates :email, length: { in: 4..30 }, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
