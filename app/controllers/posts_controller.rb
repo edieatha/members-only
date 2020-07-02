b87407ef9b3227c36f0b7431984a318b4fca4f68
Edie Atha <edieatha@gmail.com>
2020-07-02 18:38:39 +0300

    Added partial for navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,6 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
-
   def new
     @post = current_user.posts.build
   end
